0387213e575bcaac63a10065372740d31e97271d
Laxman Poudel <laxmanpoudel071@gmail.com>
2020-09-30 00:22:10 +0545

    Create Conductor.js

---
+++ Props/Conductor.js
@@ -0,0 +1,19 @@
+class Conductor {
+    constructor(x, y, w, h ,initialNegCharge, initialPosCharge) {
+        this.position = createVector(x, y);
+        this.width = w;
+        this.height = h;
+        this.negitive = initialNegCharge;
+        this.positive = initialPosCharge; 
+    }
+
+    Start(){
+        
+    }
+
+    Update() {
+
+    }
+
+    // TODO Render conductor
+}
